0b80d6c93d0e77050ceb4f1e89925af518a6e34c
Jorge Téllez <novohispano@gmail.com>
2016-09-19 14:16:04 -0600

    changes start date to local time zone

--- app/helpers/event_helper.rb
+++ app/helpers/event_helper.rb
@@ -1,5 +1,4 @@
 module EventHelper
-
   def venue_google_maps_url(venue)
     'https://maps.google.com/?q=' + URI::encode(venue['name'] + ' ' + venue['address']['localized_address_display'])
   end
@@ -21,8 +20,8 @@ module EventHelper
   end
 
   private
-  def event_start_time(event)
-    Time.parse(event['start']['utc'])
-  end
 
+  def event_start_time(event)
+    Time.parse(event['start']['local'])
+  end
 end
